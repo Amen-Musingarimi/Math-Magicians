46d2f1949613040f51d0345a1f81a2990165e917
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-08-07 22:26:01 +0200

    Add responsiveness to Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { FiMenu } from 'react-icons/fi';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Quotes from './components/QuoteApi';
 import Calculator from './components/Calculator';
@@ -11,12 +11,18 @@ import { toggleNav } from './redux/sideBarSlice';
 import './App.css';
 
 const App = () => {
-  const location = useLocation();
-  const hideNavigation = location.pathname === '/';
   const dispatch = useDispatch();
+  const isOpen = useSelector((state) => state.nav.isOpen);
+  console.log(isOpen);
+
+  const handleMainContainerClick = () => {
+    if (isOpen) {
+      dispatch(toggleNav());
+    }
+  };
 
   return (
-    <div className="main-container">
+    <div className="main-container" onClick={handleMainContainerClick}>
       <FiMenu className="menu-icon" onClick={() => dispatch(toggleNav())} />
 
       <SideBar />
